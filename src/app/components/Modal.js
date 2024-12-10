13f0d12fb3da9ac2790a5de497e26a6ab606e789
maria-studiodialect <117363135+maria-studiodialect@users.noreply.github.com>
2024-12-10 12:41:42 +0000

    Update Modal.js

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -4,7 +4,7 @@ import { IoIosClose } from "react-icons/io";
 export default function Modal({ children, onClose }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 mx-4">
-      <div className="bg-gray-800 p-4 rounded-lg shadow-lg flex flex-col">
+      <div className="bg-[#212936] p-4 rounded-lg shadow-lg flex flex-col">
       <button
           onClick={onClose}
           className=" text-white text-right self-end text-3xl mb-4"
